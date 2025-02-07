8b7a207dc84dc38bb92bd32c08f6af2aa28e6ec8
Abid Hasan <hasan.tng33@gmail.com>
2025-02-07 22:02:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// Function to show the popup
-function addRecommendation() {
-    // Show the modal
-    const modal = document.getElementById("confirmationModal");
-    modal.style.display = "flex";
-    
-    // Close the modal when the user clicks on the "x" (close button)
-    const closeBtn = document.getElementsByClassName("close")[0];
-    closeBtn.onclick = function() {
-      modal.style.display = "none"; // Hide the modal when clicked
-    }
-    
-    // Also close the modal if the user clicks anywhere outside of the modal
-    window.onclick = function(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    }
-  }
+// Function to display a message when the user clicks the mail button
+document.addEventListener("DOMContentLoaded", function() {
+    const mailButton = document.querySelector(".mail-button");
+    mailButton.addEventListener("click", function() {
+        alert("Redirecting to your email app to send an email!");
+    });
+});
+
